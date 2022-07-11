ad3243cab18f643de2d5665435a7eedd2f220a56
Sean Fidler <62626069+sfidler11@users.noreply.github.com>
2022-07-11 12:52:44 -0700

    Add files via upload

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,16 +1,14 @@
 import React from "react";
 import CreateDeck from "../CreateDeck/CreateDeck";
-import CreateDeckButton from "../CreateDeck/CreateDeckButton";
 import { Link, Route, Switch } from "react-router-dom";
 
 function Home() {
     return (
     <div>
-                <Link to="/decks/new" >
+        <Link to="/decks/new" >
             <button className="btn btn-secondary"> + Create Deck </button>
         </Link>
     <Switch>
-
         <Route path="/decks/new" >
             <CreateDeck />
         </Route>
