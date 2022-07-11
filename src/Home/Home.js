1cf1f9ba28b4d6eccf285015b070899259ae7acf
Sean Fidler <62626069+sfidler11@users.noreply.github.com>
2022-07-11 12:10:48 -0700

    Add files via upload

---
+++ src/Home/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import CreateDeck from "../CreateDeck/CreateDeck";
+import CreateDeckButton from "../CreateDeck/CreateDeckButton";
+import { Link, Route, Switch } from "react-router-dom";
+
+function Home() {
+    return (
+    <div>
+                <Link to="/decks/new" >
+            <button className="btn btn-secondary"> + Create Deck </button>
+        </Link>
+    <Switch>
+
+        <Route path="/decks/new" >
+            <CreateDeck />
+        </Route>
+    </Switch>
+    </div>
+    )
+}
+
+export default Home;
---
+++ src/Layout/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Header from "./Header";
+import NotFound from "./NotFound";
+import Home from "../Home/Home";
+
+function Layout() {
+  return (
+    <>
+    
+      <Header />
+      <div className="container">
+        <Switch>
+          <Route exact path="/">
+             <Home />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route> 
+        </Switch>
+      </div>
+    </>
+  );
+}
+
+export default Layout;
